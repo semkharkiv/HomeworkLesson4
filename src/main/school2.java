d5397bcff348c6290b6d62d97f8132b8150cab31
abdullayevsema <info.pelikantobacco@gmail.com>
2022-12-12 11:42:01 +0100

    Initial commit

---
+++ src/main/school2.java
@@ -0,0 +1,24 @@
+package main;
+
+public class school2 {
+    public static void main(String[] args) {
+
+      String text1 = "Alles gut!";
+              System.out.println(text1);
+
+ int a = 782;
+ System.out.println( a/100 );
+ System.out.println( a/10 % 10);
+ System.out.println( a%10);
+
+ System.out.println("Следующая цифра");
+
+
+ int b = 7534;
+ System.out.println(b/1000 );
+ System.out.println(b/100 % 10);
+ System.out.println(b/10 % 10);
+ System.out.println(b%10);
+
+    }
+}
